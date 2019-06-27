98df5a2523b8dc82517882f5f0c3f528567b639b
joseph townshend <joetownshend@mac.com>
2019-06-27 16:29:49 +0100

    JQuery set up / buttons returning alert messages

---
+++ src/interface.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+  console.log('ready');
+
+
+  $( "#up" ).click(function() {
+    alert("temp up");
+  });
+  $( "#down" ).click(function() {
+    alert("temp down");
+  });
+  $( "#psm" ).click(function() {
+    alert("psm");
+  });
+  $( "#reset" ).click(function() {
+    alert("reset");
+  });
+  $( "#energy" ).click(function() {
+    alert("energy");
+  });
+});
