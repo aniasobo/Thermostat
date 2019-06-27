961c31b68259dd78ced54c987a81c5a1ef41176a
Anna Sobolewska <sobolewska.a.m@gmail.com>
2019-06-27 16:46:11 +0100

    Buttons hooked up to functions

--- src/interface.js
+++ src/interface.js
@@ -1,20 +1,35 @@
 $(document).ready(function() {
   console.log('ready');
-
+  var term = new Thermostat();
 
   $( "#up" ).click(function() {
+    term.up();
+    var returnval = term.returnTemperature();
     alert("temp up");
+    console.log(returnval);
   });
+
   $( "#down" ).click(function() {
+    term.down();
+    returnval = term.returnTemperature();
     alert("temp down");
+    console.log(returnval);
   });
+
   $( "#psm" ).click(function() {
+    term.togglePSM();
     alert("psm");
+    console.log(term.isPSMon());
   });
+
   $( "#reset" ).click(function() {
+    term.reset();
     alert("reset");
+    console.log(term.returnTemperature());
   });
+
   $( "#energy" ).click(function() {
-    alert("energy");
+    var status = term.checkEnergyUsage();
+    alert(`${status}`);
   });
 });
